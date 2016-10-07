3a7407a78b0f2d013918fab0c7a4853ed1115c52
Briken <brokenbriken@gmail.com>
2016-10-07 14:35:42 +0100

    Touch input
    have touches being registered so we can see some data when the screen is
    pressed. onschedule for movement by sunday

--- Project Stranded Test/Assets/MovementScript.cs
+++ Project Stranded Test/Assets/MovementScript.cs
@@ -48,7 +48,24 @@ public class MovementScript : MonoBehaviour {
 
     void MoveFromTouch(Vector3 targetPoint, Vector3 velocity)
     {
+        
+    }
 
+    void OnGUI()
+    {
+
+        foreach (Touch touch in Input.touches)
+        {
+            string message = "";
+            message += "ID: " + touch.fingerId + "/n";
+            message += "Phase: " + touch.phase.ToString() + "/n";
+            message += "TapCount: " + touch.tapCount + "/n";
+            message += "Pos X: " + touch.position.x + "/n";
+            message += "Pos Y: " + touch.position.y + "/n";
+
+            int num = touch.fingerId;
+            GUI.Label(new Rect(0 + 130 * num, 0, 120, 100), message);
+        }
     }
 
 }
