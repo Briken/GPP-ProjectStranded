0b5715ea42c9f2e4e434f8f2cc44b4bdaa427d96
Briken <brokenbriken@gmail.com>
2016-10-16 14:31:16 +0100

    PUN netcode
    joing server and lobby using the phoon unity networking api. will soon
    have a few more features added

---
+++ Project Stranded Test/Assets/Scripts/Not UI/PhotonNetCode.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using Photon;
+
+public class PhotonNetCode : Photon.PunBehaviour {
+
+    public Text debug;
+
+	// Use this for initialization
+	void Start ()
+    {
+        PhotonNetwork.logLevel = PhotonLogLevel.Full;
+        PhotonNetwork.ConnectUsingSettings("0.1");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Debug.Log(PhotonNetwork.connectionStateDetailed.ToString());
+        //debug.text = PhotonNetwork.connectionStateDetailed.ToString();
+    }
+
+    void OnGui()
+    {
+        GUILayout.Label(PhotonNetwork.connectionStateDetailed.ToString());
+    }
+
+    public override void OnJoinedLobby()
+    {
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    void OnPhotonRandomJoinFailed()
+    {
+        debug.text = "Can't join random room";
+        PhotonNetwork.CreateRoom(null);
+    }
+
+    void OnJoinedRoom()
+    {
+        GameObject monster = PhotonNetwork.Instantiate("Square", Vector3.zero, Quaternion.identity, 0);
+    }
+}
