c4c925ba6cb525497b12acf737b2e710b0b39874
Lyall Campbell <lyallcampbell@gmail.com>
2016-11-04 12:00:13 +0000

    Positioning and sprite rotation for off-screen indicator script
    The script should now position and rotate the indicator object / sprite
    accordingly... but the position is somewhat incorrect because I didn't
    maths properly! Adds a temporary arrow sprite too for testing rotation
    functionality. (Needs tidying)

--- Project Stranded Test/Assets/Scripts/UI/UIPointOfInterest.cs
+++ Project Stranded Test/Assets/Scripts/UI/UIPointOfInterest.cs
@@ -7,10 +7,15 @@ public class UIPointOfInterest : MonoBehaviour {
     Vector3 screenSpacePosition;
     Vector3 correctedScreenSpacePosition;
 
+    public GameObject indicatorIcon;
+    public float iconPaddingAmount = 0;
+
     public GameObject gameCamera;
     Camera cameraObject;
 
     float lineGradient;
+
+
 	// Use this for initialization
 	void Start ()
     {
@@ -24,19 +29,28 @@ public class UIPointOfInterest : MonoBehaviour {
     {
         screenSpacePosition = cameraObject.WorldToScreenPoint(gameObject.transform.position);
         correctedScreenSpacePosition = new Vector3(screenSpacePosition.x - (Screen.width / 2), screenSpacePosition.y - (Screen.height / 2));
-        // Debug.Log(screenSpacePosition.ToString());
 
-        
+        // y = mx -> m = y/x
+        lineGradient = correctedScreenSpacePosition.y / correctedScreenSpacePosition.x;     
 
         if (IsOffScreen())
         {
-            Debug.Log("Object is OFF SCREEN at: " + correctedScreenSpacePosition.ToString() + " with gradient: " +(correctedScreenSpacePosition.y / correctedScreenSpacePosition.x).ToString());
+            // Debug.Log("Object is OFF SCREEN at: " + correctedScreenSpacePosition.ToString() + " with gradient: " + lineGradient.ToString() + " . Indicator position: " + DetermineIndicatorLocation().ToString());
         }
         else
         {
-            Debug.Log("Object is ON SCREEN at: " + correctedScreenSpacePosition.ToString() + " with gradient: " + (correctedScreenSpacePosition.y / correctedScreenSpacePosition.x).ToString());
+            DetermineIndicatorLocation();
+            // Debug.Log("Object is ON SCREEN at: " + correctedScreenSpacePosition.ToString() + " with gradient: " + lineGradient.ToString() + " . Indicator position: " + DetermineIndicatorLocation().ToString());
         }
 
+        // Rotate towards the target object
+        Quaternion indicatorIconRotation = Quaternion.LookRotation(gameObject.transform.position - indicatorIcon.gameObject.transform.position, Vector3.back);
+
+        // Change the rotation initially then clamp out the X and Y values (this might be a nasty way of appeasing the Quaternion gods?)
+        indicatorIcon.gameObject.transform.rotation = indicatorIconRotation;
+        indicatorIcon.gameObject.transform.rotation = Quaternion.Euler(0, 0, indicatorIcon.gameObject.transform.rotation.eulerAngles.z);
+
+        indicatorIcon.gameObject.transform.position = cameraObject.ScreenToWorldPoint(new Vector3(DetermineIndicatorLocation().x + (Screen.width / 2), DetermineIndicatorLocation().y + (Screen.height / 2), 0));
     }
 
     bool IsOffScreen()
@@ -52,5 +66,34 @@ public class UIPointOfInterest : MonoBehaviour {
         {
             return true;
         }
+    }
+
+    Vector3 DetermineIndicatorLocation()
+    {
+        // Used to construct the output vector
+        float indicatorPositionX = 0;
+        float indicatorPositionY = 0;
+
+        // Determine where the offscreen target is and get relative position on the edge of the screen, clamping if required.
+        // y = mx
+        if (correctedScreenSpacePosition.x > (Screen.width / 2))
+        {
+            indicatorPositionX = (Screen.width / 2) - iconPaddingAmount;
+            indicatorPositionY = Mathf.Clamp(lineGradient * (Screen.width / 2), -(Screen.height / 2) + iconPaddingAmount, (Screen.height / 2) - iconPaddingAmount);        
+        }
+        else if (correctedScreenSpacePosition.x < -(Screen.width / 2))
+        {
+            indicatorPositionX = -(Screen.width / 2) + iconPaddingAmount;
+            indicatorPositionY = Mathf.Clamp(lineGradient * -(Screen.width / 2), -(Screen.height / 2) + iconPaddingAmount, (Screen.height / 2) - iconPaddingAmount);           
+        }
+        else
+        {
+            indicatorPositionX = Mathf.Clamp(correctedScreenSpacePosition.x, -(Screen.width / 2) + iconPaddingAmount, (Screen.width / 2) - iconPaddingAmount);
+            indicatorPositionY = Mathf.Clamp(correctedScreenSpacePosition.y, -(Screen.height / 2) + iconPaddingAmount, (Screen.height / 2) - iconPaddingAmount);
+        }
+
+        Vector3 indicatorLocation = new Vector3(indicatorPositionX, indicatorPositionY, 0);
+
+        return indicatorLocation; 
     }
 }
