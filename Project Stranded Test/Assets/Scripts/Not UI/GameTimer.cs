b9fc273a3a4025a31c166e032b734c9118a63d5f
Briken <brokenbriken@gmail.com>
2016-11-08 16:22:08 +0000

    timer
    timer is in place now

--- Project Stranded Test/Assets/Scripts/Not UI/GameTimer.cs
+++ Project Stranded Test/Assets/Scripts/Not UI/GameTimer.cs
@@ -1,30 +1,66 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
+using System.Collections.Generic;
 using Photon;
 
 public class GameTimer : Photon.PunBehaviour {
 
 
-    public float gameTime = 300.0f;
-    float timer;
+    
+    public float timer;
     public Text UITimer;
     // Use this for initialization
+    List<GameObject> timeCheck;
+    GameTimer mainTime;
+    public GameObject roomOwner;
     void Start ()
     {
-	    
+        timer = 300.0f;
+        foreach (GameObject n in GameObject.FindGameObjectsWithTag("Player"))
+        {
+            if (n.GetComponent<MovementScript>().playerNum == 1)
+            {
+                Debug.Log("player" + n.GetComponent<MovementScript>().playerNum.ToString() + "time being used");
+                roomOwner = n;
+               // timer = n.GetComponent<GameTimer>().timer;
+            }
+            
+        }
+        //mainTime = timeCheck.GetComponent<GameTimer>();
+        //gameTime = mainTime.timer;
+        Debug.Log("time left: " + timer.ToString());
+        photonView.RPC("SetTimer", PhotonTargets.All, timer);
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-        gameTime -= Time.deltaTime;
-        photonView.RPC("SetTimer", PhotonTargets.All);
-        if (gameTime <= 0)
+        if (this.gameObject == roomOwner)
         {
-            timeEnds();
+            timer -= Time.deltaTime;
         }
-	}
+        if (roomOwner == null)
+        {
+            foreach (GameObject n in GameObject.FindGameObjectsWithTag("Player"))
+            {
+                if (n.GetComponent<MovementScript>().playerNum == 1)
+                {
+                    roomOwner = n;
+                }
+            }
+        }
+        if (this.gameObject != roomOwner)
+        {
+            timer = roomOwner.GetComponent<GameTimer>().timer;
+            Debug.Log(timer.ToString());
+        }
+        //photonView.RPC("SetTimer", PhotonTargets.All);
+        //if (gameTime <= 0)
+        //{
+        //    timeEnds();
+        //}
+    }
 
 
     public void timeEnds()
@@ -33,11 +69,21 @@ public class GameTimer : Photon.PunBehaviour {
     }
 
     [PunRPC]
-    void SetTimer()
+    float SetTimer(float masterTime)
     {
-        timer = gameTime;
-        //UITimer.text = timer.ToString();
+        if (this.gameObject.GetComponent<MovementScript>().playerNum == 1)
+        {
+            masterTime = timer;
+        }
+        else
+        {
+            timer = masterTime;
+        }
+
         Debug.Log(timer.ToString());
+
+        return timer;
+        
     }
 
     void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
