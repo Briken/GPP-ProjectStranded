d3a2045997f93e06135b9011376640d042777b8a
Lyall Campbell <lyallcampbell@gmail.com>
2016-11-21 20:50:43 +0000

    Adds initial handling for main game UI
    When used in the main game scene and set up properly, the time and fuel
    values should update accordingly.

---
+++ Project Stranded Test/Assets/Scripts/UI/UIMainGameHandler.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UIMainGameHandler : MonoBehaviour {
+
+    public GameObject timeRemainingText;
+    public GameObject fuelCarriedText;
+
+    GameObject mainPlayer;
+    float gameTimeRemaining;
+
+	// Use this for initialization
+	void Start ()
+    {
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        // Game time handling:
+        mainPlayer = GameObject.FindGameObjectWithTag("Player");
+        gameTimeRemaining = mainPlayer.GetComponent<GameTimer>().timer;
+
+        int minutes = Mathf.FloorToInt(gameTimeRemaining / 60f);
+        int seconds = Mathf.FloorToInt(gameTimeRemaining - (minutes * 60));
+        string gameTimeString = string.Format("{0:0}:{1:00}", minutes, seconds);
+
+        timeRemainingText.GetComponent<Text>().text = "TIME: " + gameTimeString;
+        fuelCarriedText.GetComponent<Text>().text = "FUEL: " + mainPlayer.GetComponent<PlayerResource>().resource.ToString();
+    }
+}
