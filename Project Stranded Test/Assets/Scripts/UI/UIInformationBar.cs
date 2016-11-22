f9ab66fcecf0f0669f3eeae37ee97ec4f0692221
Lyall Campbell <lyallcampbell@gmail.com>
2016-11-22 12:07:42 +0000

    Adjusts UI, adds information bar and power-up display functionality
    Adjusts the main game UI, hooks up some stuff (should hopefully be
    working) and adds a few new things regarding information bar and
    power-up display.

---
+++ Project Stranded Test/Assets/Scripts/UI/UIInformationBar.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UIInformationBar : MonoBehaviour {
+
+    public GameObject informationBarText;
+
+    bool isActive = false;
+    float activeTimer = 0;
+
+	// Use this for initialization
+	void Start ()
+    {
+        gameObject.GetComponent<UILerpMovement>().ReverseLerp();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (activeTimer > 0)
+        {
+            activeTimer -= Time.deltaTime;
+        }
+
+        if (isActive && (activeTimer < 0))
+        {
+            gameObject.GetComponent<UILerpMovement>().ReverseLerp();
+            isActive = false;
+        }
+	}
+
+    // Mainly use this if you want to display information!
+    public void DisplayInformationForSetTime(string message, float time)
+    {
+        informationBarText.GetComponent<Text>().text = message;
+        activeTimer = time;
+        
+        if (!isActive)
+        {
+            gameObject.GetComponent<UILerpMovement>().ActivateLerp();
+            isActive = true;
+        }   
+    }
+
+    // Manual methods:
+    public void DisplayInformation(string message)
+    {
+        informationBarText.GetComponent<Text>().text = message;
+
+        gameObject.GetComponent<UILerpMovement>().ActivateLerp();
+    }
+
+    public void HideInformation()
+    {
+        gameObject.GetComponent<UILerpMovement>().ReverseLerp();
+    }
+}
