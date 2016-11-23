c23350587d065c70ca11da4d588d413e551b1570
Lyall Campbell <lyallcampbell@gmail.com>
2016-11-23 21:37:17 +0000

    Updates UI power-up script to account for full list of power-ups
    Updates the script in advance to (optimistically) contain all of the
    intended power-ups as listed on Trello. Also includes a new freeze icon.

--- Project Stranded Test/Assets/Scripts/UI/UIPowerUp.cs
+++ Project Stranded Test/Assets/Scripts/UI/UIPowerUp.cs
@@ -45,29 +45,87 @@ public class UIPowerUp : MonoBehaviour {
     public void RevealPowerUp(int requestedPowerUpNumber)
     {
         powerUpNumber = requestedPowerUpNumber;
+        powerUpSticker.GetComponent<Image>().sprite = powerUpStickers[powerUpNumber];
+
+        gameObject.GetComponent<UILerpMovement>().ActivateLerp();
 
         switch (powerUpNumber)
         {
-            case 0:
+            case 0: // Mystery
                 powerUpSlip.GetComponent<Image>().color = new Color(140.0f / 255.0f, 0.0f / 255.0f, 150.0f / 255.0f);
-                powerUpSticker.GetComponent<Image>().sprite = powerUpStickers[0];
                 powerUpName.GetComponent<Text>().text = "???";
                 break;
 
-            case 1:
+            case 1: // Speed Boost
                 powerUpSlip.GetComponent<Image>().color = Color.red;
-                powerUpSticker.GetComponent<Image>().sprite = powerUpStickers[1];
                 powerUpName.GetComponent<Text>().text = "SPEED BOOST";
                 break;
 
-            case 2:
-                powerUpSlip.GetComponent<Image>().color = Color.magenta;
-                powerUpSticker.GetComponent<Image>().sprite = powerUpStickers[2];
-                powerUpName.GetComponent<Text>().text = "IT WORKS!!!";
+            case 2: // Bonus Fuel
+                powerUpSlip.GetComponent<Image>().color = Color.grey;
+                powerUpName.GetComponent<Text>().text = "BONUS FUEL";
+                break;
+
+            case 3: // Shield
+                powerUpSlip.GetComponent<Image>().color = Color.grey;
+                powerUpName.GetComponent<Text>().text = "SHIELD";
+                break;
+
+            case 4: // Reflect
+                powerUpSlip.GetComponent<Image>().color = Color.grey;
+                powerUpName.GetComponent<Text>().text = "REFLECT";
+                break;
+
+            case 5: // Freeze
+                powerUpSlip.GetComponent<Image>().color = new Color(0.0f / 255.0f, 135.0f / 255.0f, 255.0f / 255.0f);
+                powerUpName.GetComponent<Text>().text = "FREEZE";
+                break;
+
+            case 6: // Slow Down
+                powerUpSlip.GetComponent<Image>().color = Color.grey;
+                powerUpName.GetComponent<Text>().text = "SLOW DOWN";
+                break;
+
+            case 7: // Fuel Leak
+                powerUpSlip.GetComponent<Image>().color = Color.grey;
+                powerUpName.GetComponent<Text>().text = "FUEL LEAK";
+                break;
+
+            case 8: // Punch
+                powerUpSlip.GetComponent<Image>().color = Color.grey;
+                powerUpName.GetComponent<Text>().text = "PUNCH";
+                break;
+
+            case 9: // Outta Control
+                powerUpSlip.GetComponent<Image>().color = Color.grey;
+                powerUpName.GetComponent<Text>().text = "OUTTA CONTROL";
+                break;
+
+            case 10: // Hook
+                powerUpSlip.GetComponent<Image>().color = Color.grey;
+                powerUpName.GetComponent<Text>().text = "HOOK";
+                break;
+
+            case 11: // Switcharoo
+                powerUpSlip.GetComponent<Image>().color = Color.grey;
+                powerUpName.GetComponent<Text>().text = "SWITCHAROO";
+                break;
+
+            case 12: // Trap
+                powerUpSlip.GetComponent<Image>().color = Color.grey;
+                powerUpName.GetComponent<Text>().text = "TRAP";
+                break;
+
+            case 13: // Chicken
+                powerUpSlip.GetComponent<Image>().color = Color.grey;
+                powerUpName.GetComponent<Text>().text = "CHICKEN";
+                break;
+
+            case 14: // Spy
+                powerUpSlip.GetComponent<Image>().color = Color.grey;
+                powerUpName.GetComponent<Text>().text = "SPY";
                 break;
         }
-
-        gameObject.GetComponent<UILerpMovement>().ActivateLerp();
     }
 
     public void HidePowerUp()
