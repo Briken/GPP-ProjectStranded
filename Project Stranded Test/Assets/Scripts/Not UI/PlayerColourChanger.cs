6aaf9c88043f5ffa077f446095aa7b4fe2cc0d56
Lyall Campbell <lyallcampbell@gmail.com>
2016-11-23 23:09:42 +0000

    Adds temporary player colour changing script and materials
    Adds a script that (should) give each player a different material /
    colour when spawned. It isn't networked or all that magic therefore YOUR
    player red isn't MY player red. (...at least I think that's how it
    works!)

---
+++ Project Stranded Test/Assets/Scripts/Not UI/PlayerColourChanger.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerColourChanger : MonoBehaviour {
+
+    public Material[] playerMaterials;
+    GameObject[] playerCharacters;
+
+	// Use this for initialization
+	void Start ()
+    {
+        playerCharacters = GameObject.FindGameObjectsWithTag("Player Body");
+
+        for(int i = 0; i < playerCharacters.Length; i++)
+        {
+            playerCharacters[i].GetComponent<MeshRenderer>().material = playerMaterials[i];
+        }
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
