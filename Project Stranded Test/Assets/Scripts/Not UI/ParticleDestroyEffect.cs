80c978d7e507bd7b0b7e36622d85ba7250811137
Lyall Campbell <lyallcampbell@gmail.com>
2016-11-24 00:17:47 +0000

    Resource particle effects and setup
    Temporarily adds a little particle explosion when the resource object is
    removed. (Unity spits out a warning about not cleaning up or
    something... that'll probably have to be addressed)

---
+++ Project Stranded Test/Assets/Scripts/Not UI/ParticleDestroyEffect.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParticleDestroyEffect : MonoBehaviour {
+
+    public GameObject particleEffectPrefab;
+    public float timeUntilDestroy = 3.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnDestroy()
+    {
+        GameObject particleEffectObject = (GameObject)Instantiate(particleEffectPrefab, gameObject.transform.position, Random.rotation);
+        Destroy(particleEffectObject, timeUntilDestroy);
+    }
+}
