ed2d3601fad4bda6298e25e4164272c3a42a3188
Lyall Campbell <lyallcampbell@gmail.com>
2017-02-10 11:50:35 +0000

    Adjusts the main game UI and only displays round timer when less than 60s remain

--- Project Stranded Test/Assets/Scripts/UI/UIMainGameHandler.cs
+++ Project Stranded Test/Assets/Scripts/UI/UIMainGameHandler.cs
@@ -23,6 +23,7 @@ public class UIMainGameHandler : MonoBehaviour {
     // Use this for initialization
     void Start()
     {
+        timeRemainingText.SetActive(false);
     }
 
     // Update is called once per frame
@@ -47,8 +48,20 @@ public class UIMainGameHandler : MonoBehaviour {
         }
         int minutes = Mathf.FloorToInt(gameTimeRemaining / 60f);
         int seconds = Mathf.FloorToInt(gameTimeRemaining - (minutes * 60));
-        string gameTimeString = string.Format("{0:0}:{1:00}", minutes, seconds);
+        // string gameTimeString = string.Format("{0:0}:{1:00}", minutes, seconds);
 
-        timeRemainingText.GetComponent<Text>().text = "TIME: " + gameTimeString;
+        string gameTimeString = System.Math.Round(gameTimeRemaining, 2).ToString();
+
+        if (gameTimeRemaining > 60.0f || gameTimeRemaining == 0.0f)
+        {
+            timeRemainingText.GetComponent<Text>().text = "";
+            timeRemainingText.SetActive(false);         
+        }
+        else
+        {
+            timeRemainingText.SetActive(true);
+            timeRemainingText.GetComponent<Text>().text = "ROUND ENDS IN " + gameTimeString + "s";
+        }
+        
     }
 }
