b4959e60b83c22ad53b2bc528b2b517c3d7de4bd
Lyall Campbell <lyallcampbell@gmail.com>
2017-03-02 16:20:19 +0000

    Hooks up custom lobby naming / selection to new UI setup

---
+++ Project Stranded Test/Assets/Scripts/UI/FindRoomData.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+// Used to find and reference the persistent room data so that Unity's UI features can be used more easily
+
+public class FindRoomData : MonoBehaviour {
+
+    public GameObject roomData;
+    public InputField roomDataInputField;
+
+	// Use this for initialization
+	void Start ()
+    {
+        roomData = GameObject.FindGameObjectWithTag("GameData");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void FindRoomDataAndSetRoom()
+    {
+        // Set room name and convert to upper due to font being uppercase
+        roomData.GetComponent<RoomData>().roomName = roomDataInputField.text.ToUpper();
+    }
+}
