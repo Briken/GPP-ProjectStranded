f85ed113a457ffe7f48dea3167a39b279d81613c
Lyall Campbell <lyallcampbell@gmail.com>
2017-03-03 13:20:16 +0000

    Minor changes to round end screen and prefab. Adds quick script for changing text based on bool (needs win state hooked up)

---
+++ Project Stranded Test/Assets/Scripts/UI/UIRoundVictoryText.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIRoundVictoryText : MonoBehaviour {
+
+    public GameObject roundStateTextBox;
+    public bool playerWon;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+        if (playerWon)
+        {
+            roundStateTextBox.GetComponent<Text>().text = "YOU WON";
+        }
+        else
+        {
+            roundStateTextBox.GetComponent<Text>().text = "YOU LOST";
+        }
+		
+	}
+}
