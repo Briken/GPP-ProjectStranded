63dd1ebed4772d42d33b8ec306779b7ee47b45c8
Lyall Campbell <lyallcampbell@gmail.com>
2017-03-15 14:39:12 +0000

    Temporarily changes sonar sprite, adjusts script to use delta time and adds adjustable fade out

--- Project Stranded Test/Assets/Scripts/Not UI/GrowthCircle.cs
+++ Project Stranded Test/Assets/Scripts/Not UI/GrowthCircle.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class GrowthCircle : MonoBehaviour {
 
     public Vector3 growthRate = new Vector3(0.1f, 0.1f, 0.1f);
+    public float opacityReductionSpeed = 1.0f;
+
 
 	// Use this for initialization
 	void Start () {
@@ -14,6 +16,9 @@ public class GrowthCircle : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-        transform.localScale += growthRate;
-	}
+        transform.localScale += growthRate*Time.deltaTime;
+
+        gameObject.GetComponent<SpriteRenderer>().color = new Color(gameObject.GetComponent<SpriteRenderer>().color.r, gameObject.GetComponent<SpriteRenderer>().color.g, gameObject.GetComponent<SpriteRenderer>().color.b, gameObject.GetComponent<SpriteRenderer>().color.a - (opacityReductionSpeed*Time.deltaTime));
+
+    }
 }
