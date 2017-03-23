c88e83b66b56521f1117da61044c42448ec6ca8d
Lyall Campbell <lyallcampbell@gmail.com>
2017-03-23 09:38:47 +0000

    Adds simple resource displayed number switching based on nearby player count

---
+++ Project Stranded Test/Assets/Scripts/UI/ResourceNumberSwitcher.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResourceNumberSwitcher : MonoBehaviour {
+
+    public Sprite[] numberSprites;
+    public GameObject spriteNumber;
+    public int initialNumber;
+    public int currentNumber;
+    ResourceScript attachedResourceScript;
+
+	// Use this for initialization
+	void Start ()
+    {
+
+        attachedResourceScript = gameObject.GetComponent<ResourceScript>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        spriteNumber.GetComponent<SpriteRenderer>().sprite = numberSprites[initialNumber - attachedResourceScript.nearby.Count];
+	}
+}
