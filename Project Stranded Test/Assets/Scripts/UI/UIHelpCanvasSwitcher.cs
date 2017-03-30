889ea552df0ef02b55d282c2d2766e01b524e708
Lyall Campbell <lyallcampbell@gmail.com>
2017-03-30 16:15:24 +0100

    Adds a simple script for changing to a selected canvas and adds this to the help scene

---
+++ Project Stranded Test/Assets/Scripts/UI/UIHelpCanvasSwitcher.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIHelpCanvasSwitcher : MonoBehaviour {
+
+    public GameObject[] canvasObjects;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void SwitchToCanvas(GameObject canvasToShow)
+    {
+        foreach (GameObject canvasObject in canvasObjects)
+        {
+            if (canvasToShow == canvasObject)
+            {
+                canvasObject.SetActive(true);
+            }
+            else
+            {
+                canvasObject.SetActive(false);
+            }
+        }
+    }
+}
