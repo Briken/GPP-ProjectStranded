11170a9170c02fba3023f86d681022bfbee8a45d
Lyall Campbell <lyallcampbell@gmail.com>
2017-03-31 11:13:11 +0100

    Adds some experimental background environment changes and assets

---
+++ Project Stranded Test/Assets/Scripts/Environment/EnvRotator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnvRotator : MonoBehaviour {
+
+    public float rotationSpeedX;
+    public float rotationSpeedY;
+    public float rotationSpeedZ;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        gameObject.transform.Rotate(rotationSpeedX * Time.deltaTime, rotationSpeedY * Time.deltaTime, rotationSpeedZ * Time.deltaTime);
+	}
+}
