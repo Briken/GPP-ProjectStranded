9e045af2ea9226d9f6f12307e8e12d109ea342c5
Briken <brokenbriken@gmail.com>
2017-06-01 17:07:43 +0100

    voting system test fix

--- Project Stranded Test/Assets/Scripts/Not UI/VotingSystem.cs
+++ Project Stranded Test/Assets/Scripts/Not UI/VotingSystem.cs
@@ -16,21 +16,9 @@ public class VotingSystem : Photon.PunBehaviour
     // Use this for initialization
     void Start()
     {
-
-        playerTotals = new int[GameObject.FindGameObjectsWithTag("Player").Length];
         voteCount = GameObject.FindGameObjectWithTag("VoteObj");
         voteCard = GameObject.Find("NetworkManager").GetComponent<PhotonNetCode>().voteCards;
-        Button[] tempButtons = voteCard.GetComponentsInChildren<Button>();
-        foreach (Button n in tempButtons)
-        {
-            string buttonName = n.gameObject.name;
-            int num = buttonName[6] - '0';
-            
-            if (num > playerTotals.Length)
-            {
-                n.gameObject.SetActive(false);
-            }
-        }
+
 
     }
 
@@ -43,8 +31,23 @@ public class VotingSystem : Photon.PunBehaviour
     public void CallVote()
     {
         voteCard.SetActive(true);
-        
 
+        if (playerTotals == null)
+        {
+            playerTotals = new int[GameObject.FindGameObjectsWithTag("Player").Length];
+        }
+
+        Button[] tempButtons = voteCard.GetComponentsInChildren<Button>();
+        foreach (Button n in tempButtons)
+        {
+            string buttonName = n.gameObject.name;
+            int num = buttonName[6] - '0';
+
+            if (num > playerTotals.Length)
+            {
+                n.gameObject.SetActive(false);
+            }
+        }
     }
 
     public int CheckVote(int seed)
