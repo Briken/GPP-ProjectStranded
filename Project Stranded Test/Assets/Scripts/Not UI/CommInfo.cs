66b27be500225675da5274a09ac35602a5a85322
Briken <brokenbriken@gmail.com>
2017-08-16 22:11:47 +0100

    anotherr event

--- Project Stranded Test/Assets/Scripts/Not UI/CommInfo.cs
+++ Project Stranded Test/Assets/Scripts/Not UI/CommInfo.cs
@@ -1,13 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Analytics;
 
 public class CommInfo : MonoBehaviour {
 
     public GameObject myPlayer;
+    public Vector3 initialPos;
+    public float duration;
+    public bool eventFired;
 
 	// Use this for initialization
 	void Start () {
+
         foreach (GameObject n in GameObject.FindGameObjectsWithTag("Player"))
         {
             if (n.gameObject.GetComponent<MovementScript>().photonView.isMine)
@@ -15,11 +20,23 @@ public class CommInfo : MonoBehaviour {
                 myPlayer = n;
             }
         }
+
+        initialPos = myPlayer.transform.position;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+    {
+        duration += Time.deltaTime;
+        if (myPlayer.transform.position != initialPos && eventFired == false)
+        {
+            Analytics.CustomEvent("Player Moved After Communicating", new Dictionary<string, object>
+             {
+                 { "Player moved", myPlayer.GetComponent<MovementScript>().publicUsername + " Moved after " + duration.ToString() + " seconds"},
+                 { "Timestamp: ", System.DateTime.Now.ToString()},
+             });
+            eventFired = true;
+        }
 	}
 
     private void OnTriggerEnter(Collider other)
